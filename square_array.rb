9b4e95cf32e2cef57ab4ba0c05c8b0bae77c7cf1
boruch d <dubywider@gmail.com>
2019-03-31 21:37:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(numbers)
-  # your code here	  new_numbers = []
- 	  numbers.each do |num|
+  	  new_numbers = []
+ 	 end numbers.each do |num|
     new_numbers << (num ** 2)
   end
   return new_numbers
+end
+
 end
